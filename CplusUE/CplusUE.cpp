5c6bcce5fd12a98b568bfcfb2810452667ff2a50
dicory <zootovary66@yandex.ru>
2021-07-23 08:40:11 +0500

    Добавьте файлы проекта.

---
+++ CplusUE/CplusUE.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+/*Создать и инициализировать переменные пройденных типов данных(short int, int, long long, char, bool, float, double).*/
+
+
+/*short int lesson = 1; // задание
+int step = 12000; // шаги
+float weight = 40.3; // Вес
+char years = '8'; // Возраст
+bool human = true; // Да или Нет
+long long birthday = 280391; // День рождение
+double height = 108.1; //рост
+
+int main()
+{
+	std::cout << lesson << std::endl;
+	std::cout << step << std::endl;
+	std::cout << weight << std::endl;
+	std::cout << years << std::endl;
+	std::cout << human << std::endl;
+	std::cout << birthday << std::endl;
+	std::cout << height << std::endl;
+	return 0;
+}*/
+
+
+
+
+
+/*Создать перечисление(enum) с возможными вариантами символов для игры в крестики - нолики.*/
+
+
+/*enum class Tictactoe1
+{
+	CROSS, //крестик
+	NOLIK, //нолик
+	EMPTY //пусто
+};*/
+
+
+
+
+
+
+/*Создать массив, способный содержать значения такого перечисления и инициализировать его.*/
+
+/*enum Tictactoe { Cross, Nolik, Empty };
+int main()
+{
+	Tictactoe player1[2] = { Cross, Empty };
+	Tictactoe player2[2] = { Nolik, Empty };
+	// Работаем с нашими массивами
+	std::cout << "Player 1: " << player1[1] << std::endl;
+	std::cout << "Player 2: " << player2[0] << std::endl;
+
+	return 0;
+}*/
+
+
